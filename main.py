eb318f999229f195292a672010d4daffaf0945b4
Max <115622248+masym10@users.noreply.github.com>
2023-04-01 19:28:20 +0300

    add file and fix code

--- main.py
+++ main.py
@@ -1,46 +1,59 @@
-import pygame
-
-pygame.init()
+from pygame import*
 
 #display setting
-window = pygame.display.set_mode((700, 500))
+window = display.set_mode((700, 500))
+window.fill((255, 255, 0))
 
 #class
 class GameSprite(sprite.Sprite):
-    def __init__(self, x, y, w, h, speed):
-
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
+    def __init__(self,image_name, x, y, width, height, speed):
+        super().__init__()
+        self.image = transform.scale(image.load(image_name), (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y 
         self.speed = speed
 
-    def move(self):
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    self.x -= speed
-                if event.key == pygame.K_w:
-                    self.y -= speed
-                if event.key == pygame.K_d:
-                    self.x += speed
-                if event.key == pygame.K_s:
-                    self.y += speed
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def update_L(self):
+        keys = key.get_pressed()
+
+        if keys[K_w]:
+            self.rect.y -= self.speed
+        if keys[K_s]:
+            self.rect.y += self.speed
+
+    def update_R(self):
+        keys = key.get_pressed()
+
+        if keys[K_UP]:
+            self.rect.y += self.speed
+        if keys[K_DOWN]:
+            self.rect.y -= self.speed
+      
 FPS = 60
 game = True
-clock  = pygame.time.Clock()
+clock  = time.Clock()
+#class realase 
+player1 = Player("player.png",  20, 200, 50, 200, 5)
+player2 = Player("player.png", 500, 350, 50, 200, 5)
 
-player1 = Sprite()
-
+ball = GameSprite("ball.png", 200,200, 50, 50, 2)
 
 #game while
 while game:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in event.get():
+        if event.type == QUIT:
             game = False
 
-    player1.move()
+    player1.reset()
+    player2.reset()
 
-    pygame.display.update()
+    player1.update_L()
+    player2.update_R()
+
+    display.update()
     clock.tick(FPS) 
